c84d3a34d38ca9743323068d7aabe20680385c57
Calvin <calvin@Chucks-Air.wireless.rit.edu>
2019-04-01 18:29:11 -0400

    made border/dividers on skill section responsive

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// JavaScript DocumentfilterSelection("all")
+// JavaScript Document
+//filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
@@ -36,8 +37,8 @@ function RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btnContainer = document.getElementByClassName("portfolioMenu");
+var btns = btnContainer.getElementsByClassName("p-item");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
