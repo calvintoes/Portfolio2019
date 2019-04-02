4acbf2973e508905b6ca6cd6898a820eb3d7bebb
Calvin <calvin@chucks-air.wireless.rit.edu>
2019-04-02 00:06:56 -0400

    Finished filtering feature

--- index.js
+++ index.js
@@ -1,48 +1,27 @@
-// JavaScript Document
-//filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-  }
+$(document).ready(function() {
+
+ $('#portfolioMenu div').click(function() {
+
+var chosenFilter = $(this).attr('data-filter');
+
+$('#portfolioMenu div').removeClass('active');
+
+$(this).addClass('active');
+
+if(chosenFilter == 'all') {
+
+  $('#contentHolder').children('div').show();
+}
+else {
+
+  $('#contentHolder').children('div:not(.' + chosenFilter + ')').hide();
+
+  $('#contentHolder').children('div.' + chosenFilter).show();
+
 }
 
-// Show filtered elements
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+return false;
 
-// Hide elements that are not selected
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1); 
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementByClassName("portfolioMenu");
-var btns = btnContainer.getElementsByClassName("p-item");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
   });
-}
+
+});
